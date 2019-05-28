a05f79ad9d665bccfd1e936f7d563896924b5312
Scathacha01 <ryabera@kksm.be>
2019-05-28 18:44:18 +0900

    remove extra lines, and add comments

--- time-record.js
+++ time-record.js
@@ -1,39 +1,27 @@
 document.addEventListener("DOMContentLoaded", function() {
   var indexItem = document.getElementsByClassName("index");
-  for(var i = 0; i < indexItem.length; i++) {
+  // indexクラスを持つ要素がclickされたときに以下の処理をする
+  for (var i = 0; i < indexItem.length; i++) {
     indexItem[i].addEventListener("click", function() {
-      var cache = document.getElementsByClassName("show");
+      // hashを得るために1msの遅延
       setTimeout(function() {
+        // urlHashは現在のページのhash
         var urlHash = location.hash;
-        var person = document.getElementsByClassName(urlHash.slice(1));
-        // console.log(person.length);
-        console.log(urlHash.slice(1));
-        console.log("cache.length = "+cache.length);
-        while (cache.length != 0) {
-          console.log("cache: "+cache[0].textContent);
-          cache[0].classList.remove("show");
-          cache = document.getElementsByClassName("show");
+        // #の除去
+        urlHash = urlHash.slice(1);
+        // cacheは直前に表示されていたDOM
+        var cache = document.getElementsByClassName("show");
+        // personは表示するDOMを値
+        var person = document.getElementsByClassName(urlHash);
+        // 表示されていた要素からshowをremove
+        for (var j = 0; j < cache.length;) {
+          cache[j].classList.remove("show");
         }
-        // for (var j = 0; j < cache.length; j++) {
-        //   console.log("cache: "+cache[j].textContent);
-        //   cache[j].classList.remove("show");
-        // }
+        // 表示する要素にshowをadd
         for (var k = 0; k < person.length; k++) {
-          console.log("person: "+person[k].textContent);
           person[k].classList.add("show");
         }
-
-        // console.log(max(cache.length, person.length));
-        // for (var tmp = 0; tmp < max(cache.length, person.length); tmp++) {
-        //   cache[tmp].classList.remove("show");
-        //   person[tmp].classList.add("show");
-        // }
-
       }, 1);
     });
   }
 });
-
-// function max(num1, num2) {
-//   return num1*(num1>=num2) + num2*(num1<num2);
-// }
