7b39ac19893b55b9883f6fead38f9f3f7481050a
Scathacha01 <ryabera@kksm.be>
2019-06-24 13:53:03 +0900

    recordを外部ファイルにしてloadしました

---
+++ load-record.js
@@ -0,0 +1,64 @@
+// 2011年度の記録
+$(".time-record table tbody").load("./record/2011/kaki.html");
+$(".time-record table tbody").load("./record/2011/kankare.html");
+$(".time-record table tbody").load("./record/2011/rikousen.html");
+$(".time-record table tbody").load("./record/2011/shunki.html");
+$(".time-record table tbody").load("./record/2011/toubu.html");
+$(".time-record table tbody").load("./record/2011/touki.html");
+$(".time-record table tbody").load("./record/2011/winter.html");
+// 2012年度の記録
+$(".time-record table tbody").load("./record/2012/kaki.html");
+$(".time-record table tbody").load("./record/2012/kankare.html");
+$(".time-record table tbody").load("./record/2012/rikousen.html");
+$(".time-record table tbody").load("./record/2012/shunki.html");
+$(".time-record table tbody").load("./record/2012/toubu.html");
+$(".time-record table tbody").load("./record/2012/touki.html");
+$(".time-record table tbody").load("./record/2012/winter.html");
+// 2013年度の記録
+$(".time-record table tbody").load("./record/2013/kaki.html");
+$(".time-record table tbody").load("./record/2013/kankare.html");
+$(".time-record table tbody").load("./record/2013/rikousen.html");
+$(".time-record table tbody").load("./record/2013/shunki.html");
+$(".time-record table tbody").load("./record/2013/toubu.html");
+$(".time-record table tbody").load("./record/2013/touki.html");
+$(".time-record table tbody").load("./record/2013/winter.html");
+// 2014年度の記録
+$(".time-record table tbody").load("./record/2014/kaki.html");
+$(".time-record table tbody").load("./record/2014/kankare.html");
+$(".time-record table tbody").load("./record/2014/rikousen.html");
+$(".time-record table tbody").load("./record/2014/shunki.html");
+$(".time-record table tbody").load("./record/2014/toubu.html");
+$(".time-record table tbody").load("./record/2014/touki.html");
+$(".time-record table tbody").load("./record/2014/winter.html");
+// 2015年度の記録
+$(".time-record table tbody").load("./record/2015/kaki.html");
+$(".time-record table tbody").load("./record/2015/kankare.html");
+$(".time-record table tbody").load("./record/2015/rikousen.html");
+$(".time-record table tbody").load("./record/2015/shunki.html");
+$(".time-record table tbody").load("./record/2015/toubu.html");
+$(".time-record table tbody").load("./record/2015/touki.html");
+$(".time-record table tbody").load("./record/2015/winter.html");
+// 2016年度の記録
+$(".time-record table tbody").load("./record/2016/kaki.html");
+$(".time-record table tbody").load("./record/2016/kankare.html");
+$(".time-record table tbody").load("./record/2016/rikousen.html");
+$(".time-record table tbody").load("./record/2016/shunki.html");
+$(".time-record table tbody").load("./record/2016/toubu.html");
+$(".time-record table tbody").load("./record/2016/touki.html");
+$(".time-record table tbody").load("./record/2016/winter.html");
+// 2017年度の記録
+$(".time-record table tbody").load("./record/2017/kaki.html");
+$(".time-record table tbody").load("./record/2017/kankare.html");
+$(".time-record table tbody").load("./record/2017/rikousen.html");
+$(".time-record table tbody").load("./record/2017/shunki.html");
+$(".time-record table tbody").load("./record/2017/toubu.html");
+$(".time-record table tbody").load("./record/2017/touki.html");
+$(".time-record table tbody").load("./record/2017/winter.html");
+// 2018年度の記録
+$(".time-record table tbody").load("./record/2018/kaki.html");
+$(".time-record table tbody").load("./record/2018/kankare.html");
+$(".time-record table tbody").load("./record/2018/rikousen.html");
+$(".time-record table tbody").load("./record/2018/shunki.html");
+$(".time-record table tbody").load("./record/2018/toubu.html");
+$(".time-record table tbody").load("./record/2018/touki.html");
+$(".time-record table tbody").load("./record/2018/winter.html");
